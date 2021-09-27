ec1ce3faa1f4cd4e1f505fdc6b3327f013115e7c
Gary <58466698+garypach@users.noreply.github.com>
2021-09-26 21:42:14 -0700

    added functional prev and next arrows

--- app.js
+++ app.js
@@ -25,3 +25,30 @@ closebtn.addEventListener('click', () => {
         hamburger.style.display ="block";
     }
 });
+
+let mobileImg = document.querySelector('.mobile-header-img');
+let desktopImg = document.querySelector('.desktop-header-img');
+
+let desktopImages = ['desktop-image-hero-1.jpg','desktop-image-hero-2.jpg','desktop-image-hero-3.jpg'];
+let mobileImages = ['mobile-image-hero-1.jpg','mobile-image-hero-2.jpg','mobile-image-hero-3.jpg'];
+
+let i = 0;
+
+function prevImage(){
+    if(i<=0){
+        i = mobileImages.length;
+    }
+    i--;
+    return setImg();
+}
+function nextImage(){
+    if(i>= mobileImages.length -1){
+        i = -1;
+    }
+    i++;
+    return setImg();
+}
+
+function setImg(){
+    return mobileImg.setAttribute('src','images/' + mobileImages[i]),desktopImg.setAttribute('srcset','images/' + desktopImages[i]);
+}
