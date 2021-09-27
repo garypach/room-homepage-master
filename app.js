a1325cc41a1c70c8f27f8fd18d9cda2a7a28483c
Gary <58466698+garypach@users.noreply.github.com>
2021-09-27 16:09:25 -0700

    added content to the header text box and changing text when arrow buttons are clicked

--- app.js
+++ app.js
@@ -34,6 +34,19 @@ let mobileImages = ['mobile-image-hero-1.jpg','mobile-image-hero-2.jpg','mobile-
 
 let i = 0;
 
+let textArray = [
+    "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+    "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+    "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
+]
+let headerArray = [
+    "Discover innovative ways to decorate",
+    "We are available all across the globe",
+    "Manufactured with the best materials"
+]
+let changeText = document.querySelector('.change-text');
+let changeHeader = document.querySelector('.change-text-header');
+
 function prevImage(){
     if(i<=0){
         i = mobileImages.length;
@@ -50,5 +63,6 @@ function nextImage(){
 }
 
 function setImg(){
-    return mobileImg.setAttribute('src','images/' + mobileImages[i]),desktopImg.setAttribute('srcset','images/' + desktopImages[i]);
+    return mobileImg.setAttribute('src','images/' + mobileImages[i]),desktopImg.setAttribute('srcset','images/' + desktopImages[i]),(changeText.textContent = textArray[i]),(changeHeader.textContent = headerArray[i]);
 }
+
