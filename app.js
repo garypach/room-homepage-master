dd18b0841a0aec633845f53a7152de879611f486
Gary <58466698+garypach@users.noreply.github.com>
2021-09-28 19:19:41 -0700

    updated mobile menu/desktop menu and about section

--- app.js
+++ app.js
@@ -3,6 +3,27 @@ const nav = document.querySelector('.menu nav');
 const closebtn = document.querySelector('.close');
 
 let isNavOpen = false;
+let mobileImg = document.querySelector('.mobile-header-img');
+let desktopImg = document.querySelector('.desktop-header-img');
+
+let desktopImages = ['desktop-image-hero-1.jpg','desktop-image-hero-2.jpg','desktop-image-hero-3.jpg'];
+let mobileImages = ['mobile-image-hero-1.jpg','mobile-image-hero-2.jpg','mobile-image-hero-3.jpg'];
+
+let i = 0;
+
+let textArray = [
+    "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+    "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+    "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
+]
+let headerArray = [
+    "Discover innovative ways to decorate",
+    "We are available all across the globe",
+    "Manufactured with the best materials"
+]
+let changeText = document.querySelector('.change-text');
+let changeHeader = document.querySelector('.change-text-header');
+
 
 hamburger.addEventListener('click', () => {
     if (isNavOpen) {
@@ -26,27 +47,6 @@ closebtn.addEventListener('click', () => {
     }
 });
 
-let mobileImg = document.querySelector('.mobile-header-img');
-let desktopImg = document.querySelector('.desktop-header-img');
-
-let desktopImages = ['desktop-image-hero-1.jpg','desktop-image-hero-2.jpg','desktop-image-hero-3.jpg'];
-let mobileImages = ['mobile-image-hero-1.jpg','mobile-image-hero-2.jpg','mobile-image-hero-3.jpg'];
-
-let i = 0;
-
-let textArray = [
-    "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
-    "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
-    "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
-]
-let headerArray = [
-    "Discover innovative ways to decorate",
-    "We are available all across the globe",
-    "Manufactured with the best materials"
-]
-let changeText = document.querySelector('.change-text');
-let changeHeader = document.querySelector('.change-text-header');
-
 function prevImage(){
     if(i<=0){
         i = mobileImages.length;
@@ -66,3 +66,11 @@ function setImg(){
     return mobileImg.setAttribute('src','images/' + mobileImages[i]),desktopImg.setAttribute('srcset','images/' + desktopImages[i]),(changeText.textContent = textArray[i]),(changeHeader.textContent = headerArray[i]);
 }
 
+const breakpoint = 1024;
+window.onresize = () => {
+    if (window.innerWidth >= breakpoint) {
+        nav.style.display = 'block'
+    } else {
+        nav.style.display = 'none';
+    }
+};
